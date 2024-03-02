ddea077980740652952a4ba3e3a04490f67b899a
vic <victorgt2406@github.com>
2024-03-02 22:12:39 +0100

    api config

--- middleware/app/main.py
+++ middleware/app/main.py
@@ -1,9 +1,7 @@
 from importlib import import_module
 import os
-from typing import Union
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -18,22 +16,27 @@ def load_routes():
     for file in files:
         route_name = file.split("/")[-1][:-3]
         print(route_name)
-        module_path = file[2:-3].replace("/", ".")
-        print(module_path)
-        module = import_module(module_path)
+        module_path = file[:-3]
+        print(f"routes.{module_path}")
+        module = import_module(f"routes.{module_path}")
         app.include_router(module.router, prefix=f"/api/{route_name}")
 
 load_routes()
 
-# Client
+for route in app.routes:
+    print(1, route)
+
+# Client catch-all route for SPA
 @app.get("/{full_path:path}")
 async def catch_all(request: Request, full_path: str):
+    print("in catch all")
     if '.' in full_path:
         return FileResponse(f"./client/dist/{full_path}")
-    else:
-        return FileResponse("./client/dist/index.html")
+    return FileResponse("./client/dist/index.html")
 
+for route in app.routes:
+    print(2, route)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=8000)
