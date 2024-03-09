b5854f40d6195a9643150366b03f682eb5db0acc
vic <victorgt2406@github.com>
2024-03-09 15:46:19 +0100

    Image and phi-2 running in container

--- llm/phi-2-test5.py
+++ llm/phi-2-test5.py
@@ -3,6 +3,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Set the default device to GPU if available
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device {device}")
 torch.set_default_device(device)
 
 # Load the model and tokenizer
@@ -15,26 +16,37 @@ if tokenizer.pad_token is None:
 
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", trust_remote_code=True).to(device)
 
-pre_context = "The following is a series of questions and answers on various topics:"
-
 # Function to ask a question with pre-context
-def ask_question_with_context(question):
-    # Tokenize the pre-context and the question together
-    encoded_input = tokenizer(pre_context + "\nQ: " + question + "\nA:", return_tensors='pt', padding=True, truncation=True, max_length=256).to(device)
+def give_context(context:str):
+    encoded_input = tokenizer(context, return_tensors='pt', padding=True, truncation=True, max_length=256).to(device)
     
     # Generate an answer from the model
     outputs = model.generate(**encoded_input, max_length=256, pad_token_id=tokenizer.eos_token_id, temperature=0.7, top_k=50)
 
     # Decode the generated tokens to a string and print the answer
     answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(answer)
+
+# Function to ask a question with pre-context
+def answer(question:str):
+    # Tokenize the pre-context and the question together
+    encoded_input = tokenizer("User: " + question + "\nAssistant: ", return_tensors='pt', padding=True, truncation=True, max_length=256).to(device)
+    
+    # Generate an answer from the model
+    outputs = model.generate(**encoded_input, max_length=256, pad_token_id=tokenizer.eos_token_id, temperature=0.7, top_k=50)
+
+    # Decode the generated tokens to a string and print the answer
+    res = tokenizer.decode(outputs[0], skip_special_tokens=True)
     # Output only the answer, not the pre-context
-    print(answer.split("\nA:")[-1].strip())
+    # print(answer.split("\nAssistant: ")[-1].strip())
+    print(res)
 
 # Loop to ask multiple questions
 if __name__ == "__main__":
+    give_context("You will be an assistant who is goint to be asked in spanish or english. You will answer with the same language asked. You will respond with next questions.")
     while True:
         question = input("Enter your question (or type 'quit' to exit): ")
         if question.lower() == 'quit':
             break
 
-        ask_question_with_context(question)
+        answer(question)
