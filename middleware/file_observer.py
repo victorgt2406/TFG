0dba22970585dc8480fea96f3aecd22c008826fb
vic <victorgt2406@github.com>
2024-03-25 22:33:37 +0100

    update txt and pdfs files

--- middleware/file_observer.py
+++ middleware/file_observer.py
@@ -2,22 +2,32 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from utils.fo_json import update_jsonfile
+from utils.fo_pdf import update_pdffile
+from utils.fo_text import update_textfile
 
 class MyHandler(FileSystemEventHandler):
+
+    def handle_new_change(self, src_path:str):
+        if src_path.endswith(".json"):
+            update_jsonfile(src_path)
+        elif src_path.endswith(".txt"):
+            update_textfile(src_path)
+        elif src_path.endswith(".pdf"):
+            update_pdffile(src_path)
+        elif src_path.endswith(".csv"):
+            pass
+            
+
     def on_created(self, event):
         print(f'{event.src_path} has been created!')
         if not event.is_directory:
-            print(f'{event.src_path} is a file')
-            if event.src_path.endswith(".json"):
-                update_jsonfile(event.src_path)
+            self.handle_new_change(event.src_path)
 
     
     def on_modified(self, event):
         print(f'{event.src_path} has been modified!')
         if not event.is_directory:
-            print(f'{event.src_path} is a file')
-            if event.src_path.endswith(".json"):
-                update_jsonfile(event.src_path)
+            self.handle_new_change(event.src_path)
 
 def file_observer(path='../data'):
     event_handler = MyHandler()
---
+++ middleware/utils/fo_pdf.py
@@ -0,0 +1,26 @@
+import fitz  # PyMuPDF
+from utils.fo_json import index_docs
+
+
+def update_pdffile(filepath: str) -> None:
+    try:
+        print("before", filepath)
+        pdf = fitz.open(filepath)
+        print("after", filepath)
+        file_content = ""
+        for page in pdf:  # Iterate through each page
+            file_content += page.get_text()
+        if not file_content.strip():
+            print("PDF is empty")
+            return
+
+        # Constructing the document with more structured data
+        file_name = filepath.split("/")[-1].split(".")[0]
+        doc = {
+            "_id": filepath,
+            "file_name": file_name,
+            "text": file_content
+        }
+        index_docs([doc])
+    except Exception as e:  # Catching a more general exception since PyMuPDF might raise different exceptions
+        print(f"Error processing PDF file: {e}")
