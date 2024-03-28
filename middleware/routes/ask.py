3c0ff571c25f18caa03717a7419bcf55bf182773
vic <victorgt2406@github.com>
2024-03-28 12:40:30 +0100

    finishing cicle

--- middleware/routes/ask.py
+++ middleware/routes/ask.py
@@ -1,14 +1,16 @@
 "Users ask questions"
+import json
 import os
 import aiohttp
 from fastapi import APIRouter
 from models import AskModel
-from utils import get_async_opensearch_client, search, process_llm_terms
+from utils import get_async_opensearch_client, search
 
 router = APIRouter()
 
 @router.post("/")
 async def ask(req: AskModel):
+    message = req.message
     data_str:str = req.model_dump_json()
 
     # LLM
@@ -19,18 +21,34 @@ async def ask(req: AskModel):
     os_client = get_async_opensearch_client()
 
     # Get terms
-    terms:str = ""
     async with aiohttp.ClientSession() as session:
         async with session.post(f"{llm_url}/get_terms", data=data_str, headers={"Content-Type": "application/json"}) as response:
             if response.status == 200:
-                response_data = await response.text()
-                terms =  str(response_data)
-                print(terms)
-                terms =  process_llm_terms(terms)
-                print(terms)
+                terms = await response.json()
+                terms_str = " ".join(terms)
             else:
                 return {"error": f"Upstream server responded with status {response.status}"}
     
     # Get docs
-    docs = await search(os_client, terms)
-    return docs
+    docs = await search(os_client, terms_str)
+
+    # Conclusions
+    conclusion_msg = f"""From these options : {json.dumps(docs)}
+    Which of them is the best for this situation: {message}"""
+    data_str = json.dumps({
+        "message": conclusion_msg
+    })
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{llm_url}/ask", data=data_str, headers={"Content-Type": "application/json"}) as response:
+            if response.status == 200:
+                conclusion:str = await response.json()
+                conclusion = conclusion.replace("\n"," ").strip()
+            else:
+                return {"error": f"Upstream server responded with status {response.status}"}
+
+    res = {
+        "docs": docs,
+        "terms": terms,
+        "conclusion": conclusion
+    }
+    return res
