0a900abc8c1857b3f2b85993bf8804497ea9573a
vic <victorgt2406@github.com>
2024-04-15 14:20:55 +0200

    opensearch merged with get_opensearch_client

--- middleware/utils/opensearch.py
+++ middleware/utils/opensearch.py
@@ -1,7 +1,45 @@
 """
-Search for any match with the text to OpenSearch
+OpenSearch functions to fetch and store data
 """
-from opensearchpy import AsyncOpenSearch
+import os
+from opensearchpy import OpenSearch, AsyncOpenSearch
+
+def get_opensearch_client() -> OpenSearch:
+    host = os.getenv("OS_HOST") or "opensearch-node"
+    port = int(os.getenv("OS_PORT") or "9200")
+    user = "admin"
+    password = os.getenv("OPENSEARCH_INITIAL_ADMIN_PASSWORD") or "admin"
+    auth = (user, password)
+
+    client = OpenSearch(
+        hosts = [{'host': host, 'port': port}],
+        http_compress = True, # enables gzip compression for request bodies
+        http_auth = auth,
+        use_ssl = True,
+        verify_certs = False,
+        ssl_assert_hostname = False,
+        ssl_show_warn = False
+    )
+    return client
+
+
+def get_async_opensearch_client() -> AsyncOpenSearch:
+    host = os.getenv("OS_HOST") or "opensearch-node"
+    port = int(os.getenv("OS_PORT") or "9200")
+    user = "admin"
+    password = os.getenv("OPENSEARCH_INITIAL_ADMIN_PASSWORD") or "admin"
+    auth = (user, password)
+
+    client = AsyncOpenSearch(
+        hosts = [{'host': host, 'port': port}],
+        http_compress = True, # enables gzip compression for request bodies
+        http_auth = auth,
+        use_ssl = True,
+        verify_certs = False,
+        ssl_assert_hostname = False,
+        ssl_show_warn = False
+    )
+    return client
 
 
 async def search(os_client: AsyncOpenSearch, text: str):
