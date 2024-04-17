3d92473635c08ec0eac8c826b206f86941c9fdba
vic <victorgt2406@github.com>
2024-04-17 09:49:33 +0200

    cors and type correction  get conclusion

--- middleware/main.py
+++ middleware/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4321"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
