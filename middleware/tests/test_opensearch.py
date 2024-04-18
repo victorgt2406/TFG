ea13c669bda767dea5434c9a0302d442680236e0
vic <victorgt2406@github.com>
2024-04-18 22:06:16 +0200

    reorganizing tests

--- middleware/tests/test_opensearch.py
+++ middleware/tests/test_opensearch.py
@@ -1,11 +1,9 @@
 import asyncio
 from fastapi.testclient import TestClient
-from main import app, os_client
+from main import app
+from config import os_client
 
 client = TestClient(app)
 
-
 def test_opensearch_connection():
-    def contains_any(string:str, word_list:list[str]) -> bool:
-        return any(word in string for word in word_list)
     assert asyncio.run(os_client.info())["tagline"] == "The OpenSearch Project: https://opensearch.org/"
