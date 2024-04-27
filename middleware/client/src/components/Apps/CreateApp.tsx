c7ad221f2d48e529f3ad09a2ec5deedf4a209079
vic <victorgt2406@github.com>
2024-04-27 22:00:39 +0200

    App  with description and remove button

--- middleware/client/src/components/Apps/CreateApp.tsx
+++ middleware/client/src/components/Apps/CreateApp.tsx
@@ -5,29 +5,49 @@ import { Label } from "../../@shadcn/components/ui/label";
 import { TextareaAuto } from "../TextareaAuto";
 import mdwApi from "../../utils/mdwApi";
 import type { AppModel } from "../../models/App";
+import { toast } from "sonner";
 
 export default function () {
-    const nameRef = useRef(null);
-    const descriptionRef = useRef(null);
+    const nameRef = useRef<HTMLInputElement>(null);
+    const descriptionRef = useRef<HTMLTextAreaElement>(null);
 
-    // async function handleCreateApp(){
-    //     const content:AppModel = {
-
-    //     }
-    //     mdwApi.post("/app/",content)
-    // }
+    async function handleCreateApp() {
+        const name =
+            nameRef.current && nameRef.current.value.trim() !== ""
+                ? nameRef.current.value.trim().toLowerCase()
+                : undefined;
+        const description =
+            descriptionRef.current && descriptionRef.current.value !== ""
+                ? descriptionRef.current.value
+                : undefined;
+        if (name) {
+            const content: AppModel = {
+                name,
+                description,
+            };
+            const response = await mdwApi.post("/apps/", content);
+            console.log(response)
+            toast("App created")
+        }
+    }
 
     return (
         <>
             <div className="items-center gap-1.5">
                 <Label>Name</Label>
-                <Input type="text" placeholder="Name" ref={nameRef}/>
+                <Input type="text" placeholder="Name" ref={nameRef} />
             </div>
             <div className="items-center gap-1.5">
                 <Label>Description</Label>
-                <TextareaAuto ref={descriptionRef} placeholder="Description" className="w-full"/>
+                <TextareaAuto
+                    ref={descriptionRef}
+                    placeholder="Description"
+                    className="w-full"
+                />
             </div>
-            <Button variant="default" className="mt-4">Create App</Button>
+            <Button variant="default" className="mt-4" onClick={()=>handleCreateApp()}>
+                Create App
+            </Button>
         </>
     );
 }
