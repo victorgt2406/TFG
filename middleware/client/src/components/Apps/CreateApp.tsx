b889ba48525e980a483f261d684c61c628d941c1
vic <victorgt2406@github.com>
2024-04-27 21:34:10 +0200

    create app form created

--- middleware/client/src/components/Apps/CreateApp.tsx
+++ middleware/client/src/components/Apps/CreateApp.tsx
@@ -1,11 +1,33 @@
+import { useRef } from "react";
 import { Button } from "../../@shadcn/components/ui/button";
+import { Input } from "../../@shadcn/components/ui/input";
+import { Label } from "../../@shadcn/components/ui/label";
+import { TextareaAuto } from "../TextareaAuto";
+import mdwApi from "../../utils/mdwApi";
+import type { AppModel } from "../../models/App";
 
+export default function () {
+    const nameRef = useRef(null);
+    const descriptionRef = useRef(null);
 
+    // async function handleCreateApp(){
+    //     const content:AppModel = {
 
-export default function (){
+    //     }
+    //     mdwApi.post("/app/",content)
+    // }
+
     return (
-        <Button variant="default">
-            Create App
-        </Button>
+        <>
+            <div className="items-center gap-1.5">
+                <Label>Name</Label>
+                <Input type="text" placeholder="Name" ref={nameRef}/>
+            </div>
+            <div className="items-center gap-1.5">
+                <Label>Description</Label>
+                <TextareaAuto ref={descriptionRef} placeholder="Description" className="w-full"/>
+            </div>
+            <Button variant="default" className="mt-4">Create App</Button>
+        </>
     );
 }
