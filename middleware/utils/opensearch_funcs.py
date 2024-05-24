942839cb08f615dba28fceabfa8d5a9fd96e07f9
vic <victorgt2406@github.com>
2024-05-24 17:03:17 +0200

    clearer definitions and comments

--- middleware/utils/opensearch_funcs.py
+++ middleware/utils/opensearch_funcs.py
@@ -7,6 +7,7 @@ from opensearchpy import OpenSearch, AsyncOpenSearch
 from opensearchpy.exceptions import ConnectionError as OpenSearchConnectionError
 
 def get_opensearch_client() -> OpenSearch:
+    "Returns a pre-configured synced OpenSearch client"
     host = os.getenv("OS_HOST") or "opensearch-node"
     port = int(os.getenv("OS_PORT") or "9200")
     user = "admin"
@@ -14,18 +15,18 @@ def get_opensearch_client() -> OpenSearch:
     auth = (user, password)
 
     client = OpenSearch(
-        hosts = [{'host': host, 'port': port}],
-        http_compress = True, # enables gzip compression for request bodies
-        http_auth = auth,
-        use_ssl = True,
-        verify_certs = False,
-        ssl_assert_hostname = False,
-        ssl_show_warn = False
+        hosts=[{'host': host, 'port': port}],
+        http_compress=True,  # enables gzip compression for request bodies
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False
     )
     return client
 
-
 def get_async_opensearch_client() -> AsyncOpenSearch:
+    "Returns a pre-configured async OpenSearch client"
     host = os.getenv("OS_HOST") or "opensearch-node"
     port = int(os.getenv("OS_PORT") or "9200")
     user = "admin"
@@ -33,32 +34,31 @@ def get_async_opensearch_client() -> AsyncOpenSearch:
     auth = (user, password)
 
     client = AsyncOpenSearch(
-        hosts = [{'host': host, 'port': port}],
-        http_compress = True, # enables gzip compression for request bodies
-        http_auth = auth,
-        use_ssl = True,
-        verify_certs = False,
-        ssl_assert_hostname = False,
-        ssl_show_warn = False
+        hosts=[{'host': host, 'port': port}],
+        http_compress=True,  # enables gzip compression for request bodies
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False
     )
     return client
 
 
-async def search(os_client: AsyncOpenSearch, text: str):
-    "Search terms to OpenSearch"
-    index_name = "data"
-
+async def search(os_client: AsyncOpenSearch, terms: str, index: str = "data"):
+    "Search the text terms using OpenSearch inside the `index` indicated"
+    
     body = {
         "size": 3,
         "query": {
             "query_string": {
-                "query": text,
+                "query": terms,
                 "default_field": "*"
             }
         }
     }
 
-    response = await os_client.search(body, index=index_name)
+    response = await os_client.search(body, index=index)
 
     res = list(map(
         lambda doc: {
@@ -69,6 +69,7 @@ async def search(os_client: AsyncOpenSearch, text: str):
         response["hits"]["hits"]
     ))
     return res
+
 
 def docs_to_bulkop_string(documents: list[dict]):
     """
